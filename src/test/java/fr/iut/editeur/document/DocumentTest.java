ac03713275ababebc6a59c49b79e8df5233cd804
benhalimam <matteo.benhalima@etu.umontpellier.fr>
2024-09-11 09:56:13 +0200

    modifs test

--- src/test/java/fr/iut/editeur/document/DocumentTest.java
+++ src/test/java/fr/iut/editeur/document/DocumentTest.java
@@ -12,12 +12,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DocumentTest {
 
     @Test
-    public void testMethode() {
+    public void testAJouter() {
         Document doc  =new Document();
         CommandeFactory commandeFactory = CommandeFactory.getInstance();
         CommandeInvoker commandeInvoker = CommandeInvoker.getInstance();
         commandeInvoker.executer(commandeFactory.createCommand("ajouter",doc,new String[]{"ajouter","contenu du doc"}));
         assertTrue(doc.getTexte().equals("contenu du doc"));
+    }
+
+
+    @Test
+    public void testRemplacer() {
+        Document doc  =new Document();
+        CommandeFactory commandeFactory = CommandeFactory.getInstance();
+        CommandeInvoker commandeInvoker = CommandeInvoker.getInstance();
+        commandeInvoker.executer(commandeFactory.createCommand("ajouter",doc,new String[]{"ajouter","contenu du doc"}));
+        commandeInvoker.executer(commandeFactory.createCommand("remplacer",doc,new String[]{"remplacer","0","6","test"}));
+        assertTrue(doc.getTexte().equals("test du doc"));
     }
 
 }
